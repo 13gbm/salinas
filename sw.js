c78cde30f4cc08ac7df00279a2b981f01fcc06e1
naelsoncosta-maker <naelson.costa@gmail.com>
2026-01-20 19:43:17 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,12 +1,12 @@
-const CACHE_NAME = 'portal-13gbm-github-v4'; // VERSÃO 4
+const CACHE_NAME = 'portal-13gbm-github-v5'; // VERSÃO 5
 const urlsToCache = [
   './',
   './index.html',
   './manifest.json',
+  './icone.png',  // ADICIONEI SUA IMAGEM AQUI PARA O CACHE
   'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css'
 ];
-// ... O resto do código continua igual, só mude a versão e a lista ...
+
 self.addEventListener('install', event => { self.skipWaiting(); event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))); });
 self.addEventListener('activate', event => { event.waitUntil(caches.keys().then(cacheNames => { return Promise.all(cacheNames.map(cacheName => { if (cacheName !== CACHE_NAME) { return caches.delete(cacheName); } })); })); });
 self.addEventListener('fetch', event => { event.respondWith(caches.match(event.request).then(response => response || fetch(event.request))); });
-
