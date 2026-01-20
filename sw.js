159fc63a75ef094ca3f03d12c9e7941419b8a16f
naelsoncosta-maker <naelson.costa@gmail.com>
2026-01-20 19:33:54 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'portal-13gbm-github-v3'; // VERSÃO 3
+const CACHE_NAME = 'portal-13gbm-github-v4'; // VERSÃO 4
 const urlsToCache = [
   './',
   './index.html',
@@ -9,3 +9,4 @@ const urlsToCache = [
 self.addEventListener('install', event => { self.skipWaiting(); event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))); });
 self.addEventListener('activate', event => { event.waitUntil(caches.keys().then(cacheNames => { return Promise.all(cacheNames.map(cacheName => { if (cacheName !== CACHE_NAME) { return caches.delete(cacheName); } })); })); });
 self.addEventListener('fetch', event => { event.respondWith(caches.match(event.request).then(response => response || fetch(event.request))); });
+
